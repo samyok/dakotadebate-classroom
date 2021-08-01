413628e31434aec7169eaa8e725313516ab3071c
Samyok Nepal <nepalsamyok@gmail.com>
2021-08-01 16:27:28 -0500

    add umami tracking

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Mate+SC&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Aleo:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Merriweather+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap"
+                        rel="stylesheet"
+                    />
+                    <script
+                        async
+                        defer
+                        data-website-id="1956c323-cf18-4f00-9447-5e7f5489ebb2"
+                        src="https://analytics.dakotadebate.org/umami.js"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
